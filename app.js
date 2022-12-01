eea681f0a033269d6e5617e862db1641df55e3c3
Kortney Canyon <kcanyon_2000@yahoo.com>
2022-11-30 20:01:50 -0600

    newest update

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 let gameboard = document.querySelector(".thegameboard");
+let switchPlayer = document.querySelector(".messagesection");
 let turn = 1;
 let win = -1;
-let switchPlayer = document.querySelector(".messagesection");
 
 const matrix = [
     [-1, -1, -1],
     [-1, -1, -1],
-    [-1, -1, -1],
+    [-1, -1, -1]
 ];
 
 gameboard.addEventListener("click", playAction);
 
-function playAction(event) {
+function playAction(event, row, col) {
     if (event.target.innerHTML != "")
         return;
     if (win != -1)
         return;
+
+    matrix[row, col] = turn;
+
 
     if (turn % 2 === 1) {
         event.target.innerHTML = "X";
@@ -27,27 +30,34 @@ function playAction(event) {
         turn = 1;
     }
 
+
     for (let i = 0; i < 3; i++) {
         /* this is to check the row winner*/
-        if (matrix[i][0] == matrix[i][1] == matrix[i][2])
+        if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2])
             win = matrix[i][0];
 
         /* this is to check the column winner*/
-        if (matrix[0][i] == matrix[1][i] == matrix[2][i])
+        if (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i])
             win = matrix[0][i];
     }
     /*this is to check diagonal winner*/
-    if (matrix[0][0] == matrix[1][1] == matrix[2][2])
-        win = matrix[1][1];
-    if (matrix[0][2] == matrix[1][1] == matrix[2][0])
-        win = matrix[1][1];
+    if (matrix[00] == matrix[11] == matrix[22])
+        win = matrix[11];
+    if (matrix[02] == matrix[11] == matrix[20])
+        win = matrix[11];
 
-    endgame();
+    if (win != -1) {
+        switchPlayer.innerHTML = "Player " + turn + "has won the game";
+    }
 }
+
+
+
+/*endgame();
+
 
 function endgame() {
     if (win != -1) {
         return switchPlayer.innerHTML = "Player " + turn + "has won the game";
     }
-}
-
+}*/
