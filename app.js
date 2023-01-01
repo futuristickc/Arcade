30c8a29dcace8cade82c547a9f7b02d266a70c39
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-01-01 15:30:19 -0600

    new revised working tictactoe game

--- app.js
+++ app.js
@@ -1,63 +1,190 @@
-let gameboard = document.querySelector(".thegameboard");
-let switchPlayer = document.querySelector(".messagesection");
-let turn = 1;
-let win = -1;
+//*********** DOM SELECTORS **************
+const whosTurn = document.querySelector('.playerTurn');
+const gameBoard = document.querySelectorAll('#board');
+const restart = document.querySelector('.restart');
 
-const matrix = [
-    [-1, -1, -1],
-    [-1, -1, -1],
-    [-1, -1, -1]
+
+//*********** STATE **************
+let state = {};
+const resetState = () => {
+    state.board = ["", "", "", "", "", "", "", "", ""];
+    state.players = ['', ''];
+};
+
+let currentPlayer = state.player;
+/*let currentPlayer = () => {
+    Math.floor(Math.random() * whosTurn.length);
+    whosTurn[chooseRandom].classList.toggle('selected');
+}*/
+
+//*********** GLOBAL VARIABLES **************
+let gameActive = true; //not allow anymore sqaures to be clicked when game is won or draw
+const winningMessage = () => `It's over, ${currentPlayer} is the Champion!`;
+const drawMessage = () => `It's a draw, you guys suck!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+const winningCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
 
-gameboard.addEventListener("click", playAction);
-
-function playAction(event, row, col) {
-    if (event.target.innerHTML != "")
-        return;
-    if (win != -1)
-        return;
-
-    matrix[row, col] = turn;
 
 
-    if (turn % 2 === 1) {
-        event.target.innerHTML = "X";
-        switchPlayer.innerHTML = "It is Player 2's turn";
-        turn = 2;
+// whosTurn.innerHTML = currentPlayerTurn();
+
+
+//*********** DOM MANIPULATORS **************
+const renderBoard = () => {
+    //make sure the board is clear.
+    board.innerHTML = '';
+    //iterate through the state.board
+    for (let i = 0; i < state.board.length; i++) {
+        const cell = state.board[i];
+        //create elements for each cell
+        const cellElements = document.createElement('div');
+        //add a class to that element then adding an attribute
+        cellElements.classList.add('cell');
+        cellElements.setAttribute('data-cell-index', [i]);
+        //just displaying text in the cell
+        cellElements.innerText = cell;
+        //appending(adding) the div element with class cell as a child to the main element that has the id board
+        board.appendChild(cellElements);
+        //console.log(i);
+        //console.log('cell is: ', cell);
+    };
+};
+
+const renderPlayer = () => {
+    let text;
+    //want to have text we can see to display current player
+    //conditionally render players
+    //if there are no players we want to display an input (so someone ca put thier name in)
+    if (!state.players[0] || !state.players[1]) {
+        text = `
+        <input name="player1" placeholder="Enter Player 1" />
+        <input name="player2" placeholder="Enter Player 2" />
+        <button class="start">Start Game</button>
+        `;
     } else {
-        event.target.innerHTML = "O";
-        switchPlayer.innerHTML = "It is Player 1's turn"
-        turn = 1;
+        // if we do have players
+        /*const chooseRandom = Math.floor(Math.random() * whosTurn.length);
+        whosTurn[chooseRandom].classList.toggle('selected');*/
+        text = currentPlayerTurn();
     }
+    whosTurn.innerHTML = text;
+
+};
 
 
-    for (let i = 0; i < 3; i++) {
-        /* this is to check the row winner*/
-        if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2])
-            win = matrix[i][0];
+//*********** HELPER FUNCTIONS **************
+function takeTurns(clickedCell, clickedCellIndex) {
+    state.board[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
 
-        /* this is to check the column winner*/
-        if (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i])
-            win = matrix[0][i];
-    }
-    /*this is to check diagonal winner*/
-    if (matrix[00] == matrix[11] == matrix[22])
-        win = matrix[11];
-    if (matrix[02] == matrix[11] == matrix[20])
-        win = matrix[11];
-
-    if (win != -1) {
-        switchPlayer.innerHTML = "Player " + turn + "has won the game";
-    }
+function changePlayers() {
+    currentPlayer = currentPlayer === state.players[0] ? state.players[1] : state.players[0];
+    whosTurn.innerHTML = currentPlayerTurn();
 }
 
 
-
-/*endgame();
-
-
-function endgame() {
-    if (win != -1) {
-        return switchPlayer.innerHTML = "Player " + turn + "has won the game";
+//*********** GAME LOGIC win/start/restart **************
+function whoWon() {
+    let win = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningCombo[i];
+        let a = state.board[winCondition[0]];
+        let b = state.board[winCondition[1]];
+        let c = state.board[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            win = true;
+            break
+        }
     }
-}*/
+
+    if (win) {
+        whosTurn.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let draw = !state.board.includes("");
+    if (draw) {
+        whosTurn.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    changePlayers();
+}
+
+function startGame(event) {
+    const clickedCell = event.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+    //console.log('this is state.board:', state.board);
+    if (state.board[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    takeTurns(clickedCell, clickedCellIndex);
+    whoWon();
+}
+
+function restartGame() {
+    gameActive = true;
+    const cells = document.querySelectorAll('.cell')
+    currentPlayer = state.players[0];
+    state.board = ["", "", "", "", "", "", "", "", ""];
+    whosTurn.innerHTML = currentPlayerTurn();
+    cells.forEach(cell => cell.innerHTML = "");
+}
+
+
+//*********** EVENT LISTENERS **************
+whosTurn.addEventListener('click', (event) => {
+    //console.log('this is the event from playerTurn', event.target);
+    if (event.target.className === 'start') {
+        //get the input of player 1
+        const player1Input = document.querySelector('input[name=player1]');
+        //get the value from the input
+        //console.log('this is the value: ', player1Input.value);
+        const player1Value = player1Input.value;
+        state.players[0] = player1Value;
+
+        // Doing the samething for player 2
+        const player2Input = document.querySelector('input[name=player2]');
+        //get the value from the input
+        //console.log(player2Input.value);
+        const player2Value = player2Input.value;
+        state.players[1] = player2Value;
+        //console.log('this is the value: ', player1Value);
+        //console.log('this is the value: ', player2Value);
+
+        render();
+    }
+});
+
+gameBoard.forEach(cell => cell.addEventListener('click', startGame));
+
+restart.addEventListener('click', restartGame);
+
+
+//*********** Renders **************
+const render = () => {
+    renderBoard();
+    renderPlayer();
+};
+
+
+//*********** BootStrapping **************
+resetState();
+// startGame();
+render();
